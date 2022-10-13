28315c59fcfb0926cd6d1759d35cb50f8723ce9a
JuravekJr <jurabek009developer@gmail.com>
2022-10-13 09:15:27 +0500

    Changed Firebase Server

--- src/server/firebase.js
+++ src/server/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCH8SI_APmsUOe1315DPKvta5TjAHUwz1k",
-    authDomain: "alibaba-a30c5.firebaseapp.com",
-    projectId: "alibaba-a30c5",
-    storageBucket: "alibaba-a30c5.appspot.com",
-    messagingSenderId: "630879348855",
-    appId: "1:630879348855:web:be0a98820b182ba1377e72",
-    measurementId: "G-6KJE7RQWLX"
+    apiKey: "AIzaSyA2eTPqpTp6stTIMVWdiR2el67mKcXQcIU",
+    authDomain: "alibaba-clone-2763d.firebaseapp.com",
+    projectId: "alibaba-clone-2763d",
+    storageBucket: "alibaba-clone-2763d.appspot.com",
+    messagingSenderId: "279641805146",
+    appId: "1:279641805146:web:74a2e40cbd54d31d36a363",
+    measurementId: "G-QNWZQVND9T"
 };
 
 const app = firebase.initializeApp(firebaseConfig)
